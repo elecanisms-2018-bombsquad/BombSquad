25f1c5e5f974b2e791b035198b13c8839e0cfea5
Alexander Hoppe <alexanderehoppe@gmail.com>
2018-03-12 19:02:47 -0400

    Updated lcdtest to include 3 displays

--- lcdtest/lcdtest.c
+++ lcdtest/lcdtest.c
@@ -12,29 +12,35 @@
 int16_t main(void) {
     init_clock();
 
-    init_lcd(3); // Inits 3 _LCD type objects in an array
+    // Initializes I2C on I2C3
+    i2c_init(1e3);
 
+    // Initializes LCD structs with addresses
+    _LCD lcd1, lcd2, lcd3;
+    lcd_init(&lcd1, 0x06, 'A');
+    lcd_init(&lcd2, 0x07, 'A');
+    lcd_init(&lcd3, 0x05, 'A');
 
-    lcd_clear(&lcd[0]);  // Clears _LCD objects from previous array
-    lcd_clear(&lcd[1]);
-    lcd_clear(&lcd[2]);
+    lcd_clear(&lcd1);  // Clears _LCD objects from previous array
+    lcd_clear(&lcd2);
+    lcd_clear(&lcd3);
 
-
-    char string1[17]="Testing i2creg";
+    char string1[17]="Disp1Line1";
     char* strptr1=string1;
-    char string2[17]="using old lcd.h";
+    char string2[17]="Disp1Line2";
     char* strptr2=string2;
-    char string3[17]="Photo Sprinkler";
+    char string3[17]="Disp2Line1";
     char* strptr3=string3;
-    char string4[17]="Epsilon Counter";
+    char string4[17]="Disp2Line2";
     char* strptr4=string4;
-    char string5[17]="Depth Sanitizer";
+    char string5[17]="Disp3Line1";
     char* strptr5=string5;
-    char string6[17]="Ice Thrower";
+    char string6[17]="Disp3Line2";
     char* strptr6=string6;
 
-    lcd_print2(&lcd[1],strptr1,strptr2);
-    lcd_print2(&lcd[0],strptr3,strptr4);
+    lcd_print2(&lcd1,strptr1,strptr2);
+    lcd_print2(&lcd2,strptr3,strptr4);
+    lcd_print2(&lcd3,strptr5,strptr6);
 
     while(1) {
         //
