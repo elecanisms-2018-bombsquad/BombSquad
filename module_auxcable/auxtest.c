90a4e10b09efa87e4713f1c562f38c7e112bdbe0
Logan Sweet <loganrsweet@gmail.com>
2018-04-02 18:48:37 -0400

    backbone for aux module done, not yet funcitonal

--- module_auxcable/auxtest.c
+++ module_auxcable/auxtest.c
@@ -25,126 +25,79 @@
 */
 
 #include "elecanisms.h"
-#include "usb.h"
+// #include "usb.h"
 #include <stdio.h>
 
-#define TOGGLE_LED1         0
-#define TOGGLE_LED2         1
-#define TOGGLE_LED3         2
-#define READ_SW1            3
-#define READ_SW2            4
-#define READ_SW3            5
-#define READ_A0             6
-#define SET_DUTY_VAL        7
-#define GET_DUTY_VAL        8
-#define GET_DUTY_MAX        9
+#define s1248       0   // 539
+#define s124        1   // 556
+#define s128        2   // 576
+#define s12         3   // 595
+#define s148        4   // 618
+#define s14         5   // 639
+#define s18         6   // 664
+#define s1          7   // 688      //these tw0
+#define s248        8   // 695      // are closest
+#define s24         9   // 721
+#define s28         10  // 748
+#define s2          11  // 775
+#define s48         12  // 803
+#define s4          13  // 830
+#define s8          14  // 856
+#define s           15  // 881
+#define soops       16  // other
 
-void vendor_requests(void) {
-    WORD temp;
-    uint16_t i;
 
-    switch (USB_setup.bRequest) {
-        case TOGGLE_LED1:
-            LED1 = !LED1;
-            BD[EP0IN].bytecount = 0;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case TOGGLE_LED2:
-            LED2 = !LED2;
-            BD[EP0IN].bytecount = 0;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case TOGGLE_LED3:
-            LED3 = !LED3;
-            BD[EP0IN].bytecount = 0;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case READ_SW1:
-            BD[EP0IN].address[0] = SW1 ? 1 : 0;
-            BD[EP0IN].bytecount = 1;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case READ_SW2:
-            BD[EP0IN].address[0] = SW2 ? 1 : 0;
-            BD[EP0IN].bytecount = 1;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case READ_SW3:
-            BD[EP0IN].address[0] = SW3 ? 1 : 0;
-            BD[EP0IN].bytecount = 1;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case READ_A0:
-            temp.w = read_analog(A0_AN);
-            BD[EP0IN].address[0] = temp.b[0];
-            BD[EP0IN].address[1] = temp.b[1];
-            BD[EP0IN].bytecount = 2;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case SET_DUTY_VAL:
-            OC1R = USB_setup.wValue.w;
-            BD[EP0IN].bytecount = 0;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case GET_DUTY_VAL:
-            temp.w = OC1R;
-            BD[EP0IN].address[0] = temp.b[0];
-            BD[EP0IN].address[1] = temp.b[1];
-            BD[EP0IN].bytecount = 2;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        case GET_DUTY_MAX:
-            temp.w = OC1RS;
-            BD[EP0IN].address[0] = temp.b[0];
-            BD[EP0IN].address[1] = temp.b[1];
-            BD[EP0IN].bytecount = 2;
-            BD[EP0IN].status = UOWN | DTS | DTSEN;
-            break;
-        default:
-            USB_error_flags |= REQUEST_ERROR;
-    }
-}
+void ledoff(void){ LED1 = 0; LED2 = 0; LED3 = 0; }
+
+uint16_t state;
 
 int16_t main(void) {
-    uint8_t *RPOR, *RPINR;
-
     init_elecanisms();
 
-    // Configure pin D13 to produce a 1-kHz PWM signal with a 25% duty cycle
-    // using the OC1 module.
-    D13_DIR = OUT;      // configure D13 to be a digital output
-    D13 = 0;            // set D13 low
 
-    RPOR = (uint8_t *)&RPOR0;
-    RPINR = (uint8_t *)&RPINR0;
 
-    __builtin_write_OSCCONL(OSCCON & 0xBF);
-    RPOR[D13_RP] = OC1_RP;  // connect the OC1 module output to pin D13
-    __builtin_write_OSCCONL(OSCCON | 0x40);
+while(1){
+    uint8_t val = read_analog(A0_AN);
 
-    OC1CON1 = 0x1C06;   // configure OC1 module to use the peripheral
-                        //   clock (i.e., FCY, OCTSEL<2:0> = 0b111) and
-                        //   and to operate in edge-aligned PWM mode
-                        //   (OCM<2:0> = 0b110)
-    OC1CON2 = 0x001F;   // configure OC1 module to syncrhonize to itself
-                        //   (i.e., OCTRIG = 0 and SYNCSEL<4:0> = 0b11111)
+    if(val > 500 && val < 548){state = s1248;   }
+    if(val > 548 && val < 566){state = s124;    }
+    if(val > 566 && val < 608){state = s128;    }
+    if(val > 608 && val < 629){state = s12;     }
+    if(val > 629 && val < 650){state = s148;    }
+    if(val > 650 && val < 675){state = s14;     }
+    if(val > 675 && val < 691){state = s18;     }
+    if(val > 691 && val < 708){state = s1;      }
+    if(val > 708 && val < 732){state = s248;    }
+    if(val > 732 && val < 765){state = s24;     }
+    if(val > 765 && val < 790){state = s28;     }
+    if(val > 790 && val < 816){state = s2;      }
+    if(val > 816 && val < 842){state = s48;     }
+    if(val > 842 && val < 864){state = s4;      }
+    if(val > 864 && val < 900){state = s8;      }
+    if(val > 900 && val < 920){state = s;       }
+    if(val > 920 || val < 548){state = soops;   }
 
-    OC1RS = (uint16_t)(FCY / 1e3 - 1.);     // configure period register to
-                                            //   get a frequency of 1kHz
-    OC1R = OC1RS >> 2;  // configure duty cycle to 25% (i.e., period / 4)
-    OC1TMR = 0;         // set OC1 timer count to 0
+    switch(state){
+        case soops:
+            ledoff();
+            LED1 = 1;
+        break;
 
-    USB_setup_vendor_callback = vendor_requests;
-    init_usb();
+        case s1248:
+            ledoff();
+            LED2 = 1;
+        break;
 
-    while (USB_USWSTAT != CONFIG_STATE) {
-#ifndef USB_INTERRUPT
-        usb_service();
-#endif
-    }
-    while (1) {
-#ifndef USB_INTERRUPT
-        usb_service();
-#endif
-    }
-}
+        case s124:
+            ledoff();
+            LED3 = 1;
+        break;
+
+        // default:
+        //     state = soops;
+
+    } // end of switch
+} //end of while loop
+
+
+} // end of main
--- module_auxcable/auxtest.py
+++ module_auxcable/auxtest.py
@@ -29,16 +29,8 @@ import usb.core
 class auxtest:
 
     def __init__(self):
-        # self.TOGGLE_LED1 = 0
-        # self.TOGGLE_LED2 = 1
-        # self.TOGGLE_LED3 = 2
-        # self.READ_SW1 = 3
-        # self.READ_SW2 = 4
-        # self.READ_SW3 = 5
+        self.READ_SW1 = 3
         self.READ_A0 = 6
-        self.SET_DUTY_VAL = 7
-        self.GET_DUTY_VAL = 8
-        self.GET_DUTY_MAX = 9
 
         self.dev = usb.core.find(idVendor = 0x6666, idProduct = 0x0003)
         if self.dev is None:
@@ -48,47 +40,14 @@ class auxtest:
     def close(self):
         self.dev = None
 
-    # def toggle_led1(self):
-    #     try:
-    #         self.dev.ctrl_transfer(0x40, self.TOGGLE_LED1)
-    #     except usb.core.USBError:
-    #         print "Could not send TOGGLE_LED1 vendor request."
-    #
-    # def toggle_led2(self):
-    #     try:
-    #         self.dev.ctrl_transfer(0x40, self.TOGGLE_LED2)
-    #     except usb.core.USBError:
-    #         print "Could not send TOGGLE_LED2 vendor request."
-    #
-    # def toggle_led3(self):
-    #     try:
-    #         self.dev.ctrl_transfer(0x40, self.TOGGLE_LED3)
-    #     except usb.core.USBError:
-    #         print "Could not send TOGGLE_LED3 vendor request."
-    #
-    # def read_sw1(self):
-    #     try:
-    #         ret = self.dev.ctrl_transfer(0xC0, self.READ_SW1, 0, 0, 1)
-    #     except usb.core.USBError:
-    #         print "Could not send READ_SW1 vendor request."
-    #     else:
-    #         return int(ret[0])
-    #
-    # def read_sw2(self):
-    #     try:
-    #         ret = self.dev.ctrl_transfer(0xC0, self.READ_SW2, 0, 0, 1)
-    #     except usb.core.USBError:
-    #         print "Could not send READ_SW2 vendor request."
-    #     else:
-    #         return int(ret[0])
-    #
-    # def read_sw3(self):
-    #     try:
-    #         ret = self.dev.ctrl_transfer(0xC0, self.READ_SW3, 0, 0, 1)
-    #     except usb.core.USBError:
-    #         print "Could not send READ_SW3 vendor request."
-    #     else:
-    #         return int(ret[0])
+
+    def read_sw1(self):
+        try:
+            ret = self.dev.ctrl_transfer(0xC0, self.READ_SW1, 0, 0, 1)
+        except usb.core.USBError:
+            print "Could not send READ_SW1 vendor request."
+        else:
+            return int(ret[0])
 
     def read_a0(self):
         try:
@@ -97,32 +56,3 @@ class auxtest:
             print "Could not send READ_A0 vendor request."
         else:
             return int(ret[0]) + 256 * int(ret[1])
-
-    # def set_duty_val(self, val):
-    #     try:
-    #         self.dev.ctrl_transfer(0x40, self.SET_DUTY_VAL, val)
-    #     except usb.core.USBError:
-    #         print "Could not send SET_DUTY_VAL vendor request."
-    #
-    # def get_duty_val(self):
-    #     try:
-    #         ret = self.dev.ctrl_transfer(0xC0, self.GET_DUTY_VAL, 0, 0, 2)
-    #     except usb.core.USBError:
-    #         print "Could not send GET_DUTY_VAL vendor request."
-    #     else:
-    #         return int(ret[0]) + 256 * int(ret[1])
-    #
-    # def get_duty_max(self):
-    #     try:
-    #         ret = self.dev.ctrl_transfer(0xC0, self.GET_DUTY_MAX, 0, 0, 2)
-    #     except usb.core.USBError:
-    #         print "Could not send GET_DUTY_MAX vendor request."
-    #     else:
-    #         return int(ret[0]) + 256 * int(ret[1])
-    #
-    # def set_duty(self, duty_cycle):
-    #     val = int(round(duty_cycle * self.get_duty_max() / 100.))
-    #     self.set_duty_val(val)
-    #
-    # def get_duty(self):
-    #     return 100. * self.get_duty_val() / self.get_duty_max()
